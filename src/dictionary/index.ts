39e057179db485a1fcefa1578f6b16da70286ec8
Lyman <144786072+lymansoft@users.noreply.github.com>
2025-12-12 16:06:55 +0700

    feat: implement censor + corresponding Jest tests

--- src/dictionary/index.ts
+++ src/dictionary/index.ts
@@ -1,15 +1,35 @@
-export function addWords(words: string | string[]) {
+import bl from "./blacklist.json" with { type: "json" };
+import wl from "./whitelist.json" with { type: "json" };
 
+export const blacklist = new Set<string>(bl);
+export const whitelist = new Set<string>(wl);
+
+export function addBlacklist(words: string | string[]) {
+    if (!Array.isArray(words)) words = [words];
+    words.forEach(w => blacklist.add(w));
 }
 
-export function removeWords(words: string | string[]) {
+export function removeBlacklist(words: string | string[]) {
+    if (!Array.isArray(words)) words = [words];
+    words.forEach(w => blacklist.delete(w));
+}
 
+export function overrideBlacklist(words: string | string[]) {
+    blacklist.clear();
+    (Array.isArray(words) ? words : [words]).forEach(w => blacklist.add(w));
 }
 
 export function addWhitelist(words: string | string[]) {
-
+    if (!Array.isArray(words)) words = [words];
+    words.forEach(w => whitelist.add(w));
 }
 
 export function removeWhitelist(words: string | string[]) {
+    if (!Array.isArray(words)) words = [words];
+    words.forEach(w => whitelist.delete(w));
+}
 
+export function overrideWhitelist(words: string | string[]) {
+    whitelist.clear();
+    (Array.isArray(words) ? words : [words]).forEach(w => whitelist.add(w));
 }
